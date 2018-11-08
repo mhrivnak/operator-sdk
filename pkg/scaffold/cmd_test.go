5b4082fed2402e3e5dfcefcfa43a20868c3f23cf
Michael Hrivnak <mhrivnak@redhat.com>
2018-11-08 18:12:56 -0500

    Turns on leader election by default and adds e2e tests
    Adds a readiness probe by default that reflects whether a pod is the leader.
    
    fixes #598

--- pkg/scaffold/cmd_test.go
+++ pkg/scaffold/cmd_test.go
@@ -36,6 +36,7 @@ func TestCmd(t *testing.T) {
 const cmdExp = `package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
@@ -44,6 +45,7 @@ import (
 	"github.com/example-inc/app-operator/pkg/apis"
 	"github.com/example-inc/app-operator/pkg/controller"
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
+	"github.com/operator-framework/operator-sdk/pkg/leader"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
@@ -83,6 +85,22 @@ func main() {
 		log.Error(err, "")
 		os.Exit(1)
 	}
+
+	// Become the leader before proceeding
+	leader.Become(context.TODO(), "app-operator-lock")
+
+	// Create file for readiness probe
+	f, err := os.Create("/tmp/operator-sdk-leader")
+	if err != nil {
+		log.Error(err, "")
+		os.Exit(1)
+	}
+	err = f.Close()
+	if err != nil {
+		log.Error(err, "")
+		os.Exit(1)
+	}
+	defer os.Remove(f.Name())
 
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{Namespace: namespace})
